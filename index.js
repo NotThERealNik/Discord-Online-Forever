301fe575de1e83aa8b013f49f8afaf824903eb82
Soham <nikk.git@gmail.com>
2024-06-23 00:06:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
+module.exports = async (client) => {
+    client.on('ready', async () => {
+        client.user.setPresence({
+            activities: [
+                {
+                    name: `Nik`,
+                    type: `WATCHING`
+                }
+            ],
+            status: `idle`
+        })
+        client.logger.log(`Logged in to ${client.user.tag}`, 'ready')
+    })
+
+}
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
